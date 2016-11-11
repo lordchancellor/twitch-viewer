bac6903958284ee8e56ff999ff4f14181ac14585
Christopher Ford <chris.ford@oneserve.co.uk>
2016-11-11 13:00:15 +0000

    Fairly hefty modifications to the TwitchService and ChannelsComponent to try and populate real channels from the API. None of which are currently working.

--- src/app/components/channels/channels.component.ts
+++ src/app/components/channels/channels.component.ts
@@ -10,11 +10,30 @@ import { TwitchService } from '../../services/twitch.service';
 })
 export class ChannelsComponent implements OnInit {
   channels: TwitchChannel[];
-  val: string;
-  errorMessage: any;
+  defaultChannels: string[];
+  
+  channelInfo;
+  liveStatus;
+  
+  data: string;
+  errorMessage: any[];
 
   constructor(public twitchService: TwitchService) {
-    this.channels = [
+    this.twitchService.getChannel('freecodecamp')
+      .subscribe(
+        data => this.channelInfo = data
+      );
+
+    this.twitchService.getLiveStatus('freecodecamp')
+      .subscribe(
+        data => this.liveStatus = data
+      );
+
+
+
+
+
+    /*this.channels = [
       new TwitchChannel(
         'http://www.wallpaperbetter.com/wallpaper/230/622/534/starcraft-ii-heart-of-the-swarm-1080P-wallpaper-thumb.jpg',
         true,
@@ -33,17 +52,52 @@ export class ChannelsComponent implements OnInit {
         'StarCraft II',
         'We all love StarCraft 2!!'
       )
-    ];
+    ];*/
+
   }
 
   ngOnInit() {
+    this.defaultChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
     this.twitchService.refresh().subscribe(
-      val => this.val = val,
+      val => this.data = val,
       error =>  this.errorMessage = <any>error);
+
+    for (let channel of this.defaultChannels) {
+      let logo: string;
+      let title: string;
+      let description: string;
+      let isLive: boolean;
+ 
+      this.twitchService.getChannel(channel)
+        .subscribe(res => {
+          logo = res.logo;
+          title = res.display_name;
+          description = res.status;
+        });
+
+      this.twitchService.getLiveStatus(channel)
+        .subscribe(res => {
+          isLive = !res.stream === null;
+        });
+
+      this.channels.push(
+        new TwitchChannel(logo, isLive, title, description)
+      );
+	  }
   }
 
   logTwitchData() {
-    console.log(this.val);
+    //console.log(this.data);
+    console.log(`Channel Info: `);
+    console.log(this.channelInfo);
+    console.log(`Live Status:`);
+    console.log(this.liveStatus);
+
+    console.log(this.channelInfo.logo,
+          !this.liveStatus.stream === null,
+          this.channelInfo.display_name,
+          this.channelInfo.status);
   }
 
   ngOnDestroy() {
--- src/app/services/twitch.service.ts
+++ src/app/services/twitch.service.ts
@@ -7,8 +7,9 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class TwitchService {
 
-  apiBaseUrl: string = 'https://wind-bow.hyperdev.space/twitch-api/streams/';
+  apiBaseUrl: string = 'https://wind-bow.hyperdev.space/twitch-api/';
   user: string = 'ESL_SC2';
+  //user: string = 'freecodecamp';
   query: string = '?callback=JSONP_CALLBACK';
 
   defaultChannels: string[] = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
@@ -16,11 +17,26 @@ export class TwitchService {
   constructor(public jsonp: Jsonp) { }
 
   refresh(): Observable<string> {
-    const apiCall: string = this.apiBaseUrl + this.user + this.query;
+    const apiCall: string = this.apiBaseUrl + 'streams/' + this.user + this.query;
 
     return this.jsonp.get(apiCall)
           .map((res: Response) => res.json())
-          .catch((err: any) => Observable.throw(err.json().err))
-          .do(data => console.log("All: " + JSON.stringify(data)));
+          .catch((err: any) => Observable.throw(err.json().err));
+  }
+
+  getChannel(channel: string): Observable<any> {
+    const apiCall: string = this.apiBaseUrl + 'channels/' + channel + this.query;
+
+    return this.jsonp.get(apiCall)
+        .map((res: Response) => res.json())
+        .catch((err: any) => Observable.throw(err.json().err));
+  }
+
+  getLiveStatus(channel: string): Observable<any> {
+    const apiCall: string = this.apiBaseUrl + 'streams/' + channel + this.query;
+
+    return this.jsonp.get(apiCall)
+        .map((res: Response) => res.json())
+        .catch((err: any) => Observable.throw(err.json().err));
   }
 }
